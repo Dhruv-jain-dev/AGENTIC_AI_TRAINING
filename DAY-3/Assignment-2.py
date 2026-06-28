905aee4986e52e2d535a9a84975f0fa396672dad
Dhruv-jain-dev <dhruvjainofficials@gmail.com>
2026-06-28 15:32:24 +0530

    Created Assignment-2.py

---
+++ DAY-3/Assignment-2.py
@@ -0,0 +1,34 @@
+#Cinema Seat Booking System
+cinema = {
+    101:{'movie': 'Cocktail 2',
+    'theatre': 'PVR Cinemas,
+    'screen': 'Screen 1',
+    'date': '28 July',
+    'show_time': '7:30 PM',
+    'ticket_price': 250,
+
+    'seats': [
+        ["A1", "A2", "A3", "A4", "A5"],
+        ["B1", "B2", "B3", "B4", "B5"],
+        ["C1", "C2", "C3", "C4", "C5"]
+    ],
+
+    'booked_seats': ["A2", "B4", "C1"]
+    }
+    102:{
+    'movie': 'Carry On Jatta 4',
+    'theatre': 'Cinepolis',
+    'screen': 'Screen 2',
+    'show_time': '10:15 AM',
+    'ticket_price': 180,
+
+    'seats': [
+        ["A1", "A2", "A3", "A4", "A5"],
+        ["B1", "B2", "B3", "B4", "B5"],
+        ["C1", "C2", "C3", "C4", "C5"]
+    ],
+
+    'booked_seats': ["A1", "A5", "B2", "C4"]
+    }
+    
+}
