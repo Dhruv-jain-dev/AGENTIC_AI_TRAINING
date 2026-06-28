e4a65a616ee7df56244fb6280a8acdc99a1e2e9d
Dhruv-jain-dev <dhruvjainofficials@gmail.com>
2026-06-28 15:05:08 +0530

    Created Assignment-1.py

---
+++ DAY-3/Assignment-1.py
@@ -0,0 +1,39 @@
+# Assignment_1- To create a mapping of flights as we see in popular travel apps.
+flights = {
+    '6E 846': {
+        'Airline Name': 'IndiGo',
+        'Source': 'Delhi',
+        'Destination': 'Bengaluru',
+        'Departure Time \ Day, Date': '22:00 \ Sun, 28 June',
+        'Arrival Time \ Day, Date': '00:55 \ Mon, 29 June',
+        'Ticket Price': '₹10201',
+        'Trip type':'One-way',
+        'Seats Left':'10'
+    },
+    'IX 1070':{
+        'Airline Name': 'AirIndia',
+        'Source': 'Delhi',
+        'Destination': 'Bengaluru',
+        'Departure Time \ Day, Date': '23:00 \ Sun, 28 June',
+        'Arrival Time \ Day, Date': '01:55 \ Mon, 29 June',
+        'Ticket Price': '₹20403',
+        'Trip type':'One-way',
+        'Seats Left':'10'
+    },
+    'UO 702':{
+        'Airline Name': 'Hong Kong Express Airways',
+        'Source': 'Hong Kong',
+        'Destination': 'Bangkok',
+        'Departure Time \ Day, Date': '16:55 \ Fri, 17 Jul',
+        'Arrival Time \ Day, Date': '18:55 \ Fri, 17 Jul',
+        'Ticket Price': '₹25739',
+        'Trip type':'Round-Trip',
+        'Seats Left':'10'
+        'Return_Flight': {
+            'Flight_NO': 'UO 733',
+            'Departure Time \ Day, Date': '12:55 \ Fri, 19 Jul',
+            'Arrival Time \ Day, Date': '15:05 \ Fri, 19 Jul',
+            'Seats Left':'10'
+        }
+    }
+}
