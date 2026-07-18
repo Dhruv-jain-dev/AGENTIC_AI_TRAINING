50d0283ac3e5bb354f03b7cb0e02309f8af38057
Dhruv Jain <dhruvjainofficials@gmail.com>
2026-07-18 23:58:02 +0530

    generator.py
    Implement a generator function to yield food orders.

---
+++ DAY-17/generator.py
@@ -0,0 +1,13 @@
+def deliver_orders():
+    yield 'Pizza'
+    yield 'Burger'
+    yield 'Noodles'
+
+orders = deliver_orders()
+
+print(orders)
+print(type(orders))
+
+for order in orders:
+    print(order)
+  
